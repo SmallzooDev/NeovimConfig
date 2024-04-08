a953e819a84f7754ff05cd841d6d7a91d70e92ba
Joonkyu_Kang <joonkyu_kang@kolon.com>
2024-04-08 13:11:12 +0900

    #49 revise comment

--- lua/smallzooDev/plugins/formatting.lua
+++ lua/smallzooDev/plugins/formatting.lua
@@ -14,7 +14,6 @@ return {
 				html = { "prettier" },
 				json = { "prettier" },
 				yaml = { "prettier" },
-				graphql = { "prettier" },
 				liquid = { "prettier" },
 				lua = { "stylua" },
 				python = { "isort", "black" },
