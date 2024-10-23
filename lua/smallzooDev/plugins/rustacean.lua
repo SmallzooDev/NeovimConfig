07f8089a3ac11f83089dd16ed3ca7f45dde39ec5
smallzooDev <smallzooDev@gmail.com>
2024-10-23 19:47:36 +0900

    _

--- lua/smallzooDev/plugins/rustacean.lua
+++ lua/smallzooDev/plugins/rustacean.lua
@@ -5,21 +5,6 @@ return {
 		lazy = false,
 		ft = "rust",
 		config = function()
-			local mason_registry = require("mason-registry")
-			local codelldb = mason_registry.get_package("codelldb")
-			local extension_path = codelldb:get_install_path() .. "/extension/"
-			local codelldb_path = extension_path .. "adapter/codelldb"
-			local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-			local map = vim.api.nvim_set_keymap
-			local cfg = require("rustaceanvim.config")
-
-			map("n", "<Leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = "Debugger testables" })
-			vim.g.rustaceanvim = {
-				dap = {
-					adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-				},
-			}
-
 			local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 			for type, icon in pairs(signs) do
 				local hl = "DiagnosticSign" .. type
